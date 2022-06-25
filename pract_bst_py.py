65249a974ebb2b898dcc5ce4529fedb4ba63f4fa
mHappiee <83241650+HappyBravo@users.noreply.github.com>
2022-06-25 11:43:46 +0530

    Update pract_bst_py.py

--- pract_bst_py.py
+++ pract_bst_py.py
@@ -10,25 +10,38 @@ if __name__ == "__main__":
     root2 = None
     root2 = bst.AVL()
     # root2.insert(1)
-    n = 20
 
-    arr = [random.randint(0, 99) for i in range(n)]
+    root3 = None
+    root3 = bst.RBT()
+
+    n = 3
+
+    arr = [random.randint(0, 50) for i in range(n)]
     print(arr)
     input()
     print(root)
     print(root2)
+    print(root3)
 
     for i in arr:
         print(f"\ninserting {i}")        
         # root.insert(i)
-        root2.insert(i)
-        # root.print_tree()
-        root2.print_tree()
+        # root2.insert(i)
+        root3.insert(i)
 
-        time.sleep(1.5)
-        # input()
+        # root.print_tree()
+        # root2.print_tree()
+        root3.print_tree()
+
+
+        # time.sleep(1.5)
+        input()
         # print("---")
     
+    # root2.print_tree()
+    root3.print_tree()
+
+
     # root.print(traversal_type="preorder")
     # print()
     # root.print_tree()
@@ -38,12 +51,15 @@ if __name__ == "__main__":
     # # root = None
 
     # # root.print_tree()
+    '''
+    while int(input("Do you want to remove ? (1:YES 0: NO)")):   
+        d = int(input("Enter data to remove : "))
+        # root.remove(d)
+        # root.print_tree()
+        # root.print(traversal_type="preorder")
 
-    # while int(input("Do you want to remove ? (1:YES 0: NO)")):   
-    #     d = int(input("Enter data to remove : "))
-    #     root.remove(d)
-    #     root.print_tree()
-    #     root.print(traversal_type="preorder")
+        root2.remove(d)
+        root2.print_tree()
 
     # print("---SEARCHING---")
     # while int(input("search ? : 1 or 0 : ")):
@@ -55,3 +71,5 @@ if __name__ == "__main__":
     # root2 = bst.AVL()
     # root2.insert(1)
     root2.print_tree()
+    root2.print()
+    '''
