af7ead6bc2da11002b9799b6c55ad9a0ca6b4d0b
mHappiee <83241650+HappyBravo@users.noreply.github.com>
2022-07-05 00:45:07 +0530

    Update pract_rbt.c

--- pract_rbt.c
+++ pract_rbt.c
@@ -1,20 +1,39 @@
-// #include "red_black_tree_functions.c"
 #include "rbt_functions.c"
-#include "python_functions.c"
+#include "python_functions.c" // FROM "https://github.com/HappyBravo/python_functions_in_C"
+#include <unistd.h>
+#include <time.h>
 
 int main(){
     rb_tree *root = NULL;
     char t;
-    int arr[10] = {11, 2, 20, 45, 98, 22, 62, 64, 50, 84};
-    for(int i = 0; i< 10; i++){
-        printf("\nInserting %d\n", arr[i]);
-        root = rb_insert(root, arr[i]);
-        // rb_print_tree(root);
-        // printf("\n\n");
-        // scanf("%c", &t);
+    
+    // int arr[10] = {11, 2, 20, 45, 98, 22, 62, 64, 50, 84};
+    
+    // printf("\n\t - - - - - Inserting random numbers - - - - - - \n");
+    int n;
+    srand(time(NULL));
+
+    for(int i = 0; i<10; i++){
+        // n = rand()%100;
+        printf("ENTER NUMBER : ");
+        scanf("%d", &n);
+        printf("\n\tINSERTING %d IN THE TREE.....\n\n", n);
+        // sleep(2);
+        root = rb_insert(root, n);
+
+
+        // printf("\nInserting %d\n", arr[i]);
+        // root = rb_insert(root, arr[i]);
+        
+        rb_print_tree(root);
+        printf("\n\n");
+        scanf("%c", &t);
     }
+    printf("\n\t<<<\tFINAL TREE\t>>>\n\n");
     rb_print_tree(root);
-    printf("\n<<< \tDELETING\t>>>\n");
+
+    printf("\n<<<\tDELETING\t>>>\n");
+    
     int temp;
     do{
         printf("Enter number to delete : ");
