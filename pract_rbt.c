72ab14d510e77d00b2f0751242600f1ca319319e
mHappiee <83241650+HappyBravo@users.noreply.github.com>
2022-07-04 17:14:17 +0530

    Update pract_rbt.c
    Added codes for deletions

--- pract_rbt.c
+++ pract_rbt.c
@@ -1,5 +1,6 @@
+// #include "red_black_tree_functions.c"
 #include "rbt_functions.c"
-
+#include "python_functions.c"
 
 int main(){
     rb_tree *root = NULL;
@@ -8,11 +9,19 @@ int main(){
     for(int i = 0; i< 10; i++){
         printf("\nInserting %d\n", arr[i]);
         root = rb_insert(root, arr[i]);
-        
-        rb_print_tree(root);
-        printf("\n\n");
-        scanf("%c", &t);
+        // rb_print_tree(root);
+        // printf("\n\n");
+        // scanf("%c", &t);
     }
     rb_print_tree(root);
+    printf("\n<<< \tDELETING\t>>>\n");
+    int temp;
+    do{
+        printf("Enter number to delete : ");
+        scanf("%d", &temp);
+        root = rb_remove(root, temp);
+        rb_print_tree(root);
+    }while(atoi(python_input("Delete again ? (1/0) : ")));
+    
     return 0;
 }
